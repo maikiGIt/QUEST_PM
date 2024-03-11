e45c19db29737d2410b3d561d6369be9cf427ad3
maikiGit <maiki1109@gmail.com>
2024-03-11 09:28:57 +0000

    step2 Get Password

--- password_manager.sh
+++ password_manager.sh
@@ -22,7 +22,25 @@ case $option in
 
     echo "パスワードの追加は成功しました。"
 ;;
+# Get Password が入力された場合
+"Get Password")
 
+    echo "サービス名を入力してください："
+    read service_name
+    
+    result=$(grep "$service_name:" personal_info.txt)
+    
+    if [ "$result" ]; then
+    ## サービス名が保存されていた場合
+        IFS=: read service_name user_name password <<< "$result"
+        echo "サービス名：$service_name" 
+        echo "ユーザー名：$user_name"
+        echo "パスワード：$password"
+    else
+    ## サービス名が保存されていなかった場合
+        echo "そのサービスは登録されていません。"
+    fi
+;;
 # Add Password/Get Password/Exit 以外が入力された場合
 *)
     echo "入力が間違えています。Add Password/Get Password/Exit から入力してください。"
